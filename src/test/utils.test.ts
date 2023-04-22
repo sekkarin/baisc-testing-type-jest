951332878af2cca1c3bf9dfcb297086fa29fdbae
sekkarin <sekkri1234@gmail.com>
2023-04-22 13:08:40 +0700

    multiple tests structure

--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -1,8 +1,8 @@
 import { assert } from "console";
 import { getStringInfo, toUpperCase } from "../utils";
 
-describe('Utils test suite',()=>{
-    it('should return uppercase',()=>{
+describe('Utils test suite', () => {
+    it('should return uppercase', () => {
         // arrange เตรียมข้อมูล
         const sut = toUpperCase;
         const expected = "ABC";
@@ -13,21 +13,44 @@ describe('Utils test suite',()=>{
         // assert ผลที่คาดหวัง
         expect(actual).toBe(expected)
     })
-    it('should return info for valid string',()=>{
-        const actual = getStringInfo('My-String');
-
-        expect(actual.lowerCase).toBe('my-string')
-        expect(actual.upperCase).toBe('MY-STRING')
-        expect(actual.extraInfo).toEqual({})
-        expect(actual.charaters.length).toBe(9)
-        expect(actual.charaters).toHaveLength(9)
-        expect(actual.charaters).toEqual(['M','y','-','S','t','r','i','n','g'])
-        expect(actual.charaters).toContain<string>('M') // เช็คข้อมูลใน array 
-        expect(actual.charaters).toEqual(expect.arrayContaining(['M','y','-','S','t','r','i','g','n']))
-
-        expect(actual.extraInfo).not.toBe(undefined)
-        expect(actual.extraInfo).not.toBeUndefined()
-        expect(actual.extraInfo).toBeDefined()
-        expect(actual.extraInfo).toBeTruthy()
+    describe('getstringInfo for arg My-String should', () => {
+        
+        test('return right length', () => {
+            const actual = getStringInfo('My-String')
+            expect(actual.charaters).toHaveLength(9)
+        })
+        test('return right lower case', () => {
+            const actual = getStringInfo('My-String')
+            expect(actual.lowerCase).toBe('my-string')
+        })
+        test('return right upper case', () => {
+            const actual = getStringInfo('My-String')
+            expect(actual.upperCase).toBe('MY-STRING')
+        })
+        test('return right charecters', () => {
+            const actual = getStringInfo('My-String')
+            expect(actual.charaters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g'])
+            expect(actual.charaters).toContain<string>('M') // เช็คข้อมูลใน array 
+            expect(actual.charaters).toEqual(expect.arrayContaining(['M', 'y', '-', 'S', 't', 'r', 'i', 'g', 'n']))
+        })
+        test('return right definded extra info', () => {
+            const actual = getStringInfo('My-String')
+            expect(actual.extraInfo).not.toBe(undefined)
+            expect(actual.extraInfo).not.toBeUndefined()
+            expect(actual.extraInfo).toBeDefined()
+            expect(actual.extraInfo).toBeTruthy()
+        })
     })
+    // it.only('should return info for valid string', () => {
+    //     const actual = getStringInfo('My-String');
+
+    //     expect(actual.lowerCase).toBe('my-string')
+    //     expect(actual.upperCase).toBe('MY-STRING')
+    //     expect(actual.extraInfo).toEqual({})
+    //     expect(actual.charaters.length).toBe(9)
+    //     expect(actual.charaters).toHaveLength(9)
+
+
+
+    // })
 })
